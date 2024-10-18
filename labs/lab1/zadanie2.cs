713783f1b2c9b981b97c658e885a99e9953c83f8
MikhailShapovalov1 <shapovalov2006mail.ru@gmail.com>
2024-10-18 14:44:23 +0600

    добавил задание 2

---
+++ labs/lab1/zadanie2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs.lab1
+{
+    internal class zadanie2
+    {
+        static void Main()
+        {
+            int a;
+            a = 10;
+            int b;
+            b = 6;
+            int max;
+            max = ((a + b) + Math.Abs(a - b)) / 2;
+            int min;
+            min = ((a + b) - Math.Abs(a - b)) / 2;
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
