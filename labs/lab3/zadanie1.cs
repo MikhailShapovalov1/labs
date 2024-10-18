2ec0613da93c7dd6c9a57718e7b8da8ddbb1887a
MikhailShapovalov1 <shapovalov2006mail.ru@gmail.com>
2024-10-18 20:17:04 +0600

    lab3-1

---
+++ labs/lab3/zadanie1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs.lab3
+{
+    internal class zadanie1
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                int currentNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (currentNumber > 0)
+                {
+                    int number = 0;
+                    while (currentNumber > 0)
+                    {
+                        int n = currentNumber % 10;
+                        currentNumber = (currentNumber - n) / 10;
+
+                        if (n % 2 != 0)
+                        {
+                            number = number * 10 + n;
+                        }
+                    }
+
+                    if (number != 0)
+                    {
+                        Console.WriteLine($"число: {number}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("нечетных цифр нет");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
