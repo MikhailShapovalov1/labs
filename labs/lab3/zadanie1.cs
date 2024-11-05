6f1263dc8038aa74818a5e1816979a0196424c35
MikhailShapovalov1 <shapovalov2006mail.ru@gmail.com>
2024-11-05 22:09:22 +0600

    добавил 3 лабу

---
+++ labs/lab3/zadanie1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int currentNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (currentNumber > 0)
+            {
+                int number = 0;
+                while (currentNumber > 0)
+                {
+                    int n = currentNumber % 10;
+                    currentNumber = (currentNumber - n) / 10;
+
+                    if (n % 2 != 0)
+                    {
+                        number = number * 10 + n;
+                    }
+                }
+
+                if (number != 0)
+                {
+                    Console.WriteLine($"число: {number}");
+                }
+                else
+                {
+                    Console.WriteLine("нечетных цифр нет");
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+
+
+    }
+}
---
+++ labs/lab3/zadanie2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int maxSUM = 0;
+        int currentSUM = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                currentSUM += x;
+            }
+            else
+            {
+                if (currentSUM > maxSUM)
+                {
+                    maxSUM = currentSUM;
+                }
+                currentSUM = 9;
+            }
+        }
+        if (currentSUM > maxSUM)
+        {
+            maxSUM = currentSUM;
+        }
+        Console.WriteLine("Максимальная сумма четных равна:" + maxSUM);
+    }
+}
